3dd49589a00e9dd4b285b15d367787ad7bad1f5e
Ruivo <joaolucasrr0210@gmail.com>
2025-03-14 04:25:46 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect('mongodb://localhost:27017/loja', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Conectado ao MongoDB"))
